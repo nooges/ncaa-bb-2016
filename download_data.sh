45c871ea1b3c7542fbaa0f3a70a59a5603e454fa
Danny Nguyen <danny@80pct.com>
2016-03-16 14:27:04 -0400

    Only download games for NCAA Division I

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl "http://www.masseyratings.com/scores.php?s=284067&sub=284067&all=1&mode=3&format=1" -o games_2016.csv
-curl "http://www.masseyratings.com/scores.php?s=284067&sub=284067&all=1&mode=3&format=2" -o teams_2016.csv
+# Download data for NCAA Division I
+curl "http://www.masseyratings.com/scores.php?s=284067&sub=11590&all=1&mode=3&format=1" -o data/games_2016.csv
+curl "http://www.masseyratings.com/scores.php?s=284067&sub=11590&all=1&mode=3&format=2" -o data/teams_2016.csv
