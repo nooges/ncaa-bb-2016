4d596c0729e7cdd1b8c0a80e44afd1236fb65bb4
Danny Nguyen <danny@80pct.com>
2016-03-17 10:52:49 -0400

    Generate picks for all games

--- rate_ncaa.py
+++ rate_ncaa.py
@@ -3,6 +3,7 @@
 import csv
 from datetime import datetime
 from pprint import pprint
+import random
 
 # Iterate through each game and compute rating
 class EloRater(object):
@@ -12,6 +13,8 @@ class EloRater(object):
         self.teams = {}
         self.team_name_to_id = {}
         self.games = []
+        self.max_bonus = 0
+        self.regions = ['South', 'East', 'West', 'Midwest']
 
         print 'Reading teams'
         self.read_participating_teams('data/tournament_teams.csv')
@@ -61,14 +64,22 @@ class EloRater(object):
 
     def read_participating_teams(self, teams_filename):
         self.tournament_teams = {}
+        self.seeds = {}
         with open(teams_filename) as csvfile:
             csvreader = csv.reader(csvfile)
             for row in csvreader:
-                team = {'seed': int(row[0]), 'region': row[1]}
+                seed = int(row[0])
+                region = row[1]
                 team_name = row[2]
+                team = {'seed': seed, 'region': region}
                 self.tournament_teams[team_name] = team
+                region_seeds = self.seeds.get(region, {})
+                region_seeds[seed] = team_name
+                self.seeds[region] = region_seeds
 
         pprint(self.tournament_teams)
+        pprint(self.seeds)
+
     def expected_win_probability(self, rating1, rating2):
         return 1.0/(1 + 10**((rating2 - rating1)/400))
 
@@ -142,8 +153,76 @@ class EloRater(object):
                     team[1]['seed'],
                     team[1]['name'].replace('_', ' '),)
 
+    def game_id(self, region, round_number, game_number):
+        return '{}-{}-{}'.format(region, round_number, game_number)
+
+    def generate_initial_matchups(self):
+        self.matchups = {}
+
+        # Round 1
+        for region in self.regions:
+            for game_number in xrange(8):
+                game_id = self.game_id(region, 1, game_number+1)
+                seed1 = [1,8,5,4,6,3,7,2][game_number]
+                seed2 = 17 - seed1
+                game_info = {'team1': self.seeds[region][seed1], 'team2': self.seeds[region][seed2]}
+                self.matchups[game_id] = game_info
+
+        pprint(self.matchups)
+
+    def pick_winner(self, game_id, next_game_id):
+        matchup = self.matchups[game_id]
+        team1_id = self.team_name_to_id[matchup['team1']]
+        team2_id = self.team_name_to_id[matchup['team2']]
+        team1_seed = self.teams[team1_id]['seed']
+        team2_seed = self.teams[team2_id]['seed']
+        team1_rating = self.teams[team1_id]['rating']
+        team2_rating = self.teams[team2_id]['rating']
+        team1_win_probability = self.expected_win_probability(team1_rating, team2_rating)
+        winner_selection = random.random()
+
+        # Pick team to win
+        winner_name = matchup['team1'] if team1_win_probability > winner_selection else matchup['team2']
+        next_matchup = self.matchups.get(next_game_id, {})
+        if 'team1' in next_matchup:
+            next_matchup['team2'] = winner_name
+        else:
+            next_matchup['team1'] = winner_name
+        self.matchups[next_game_id] = next_matchup
+
+        print '({5}) {0} vs ({6}) {1}: {2:.2f} {3:.2f}, {4}'.format(matchup['team1'], matchup['team2'], team1_win_probability, winner_selection, winner_name, team1_seed, team2_seed)
+
     def generate_bracket_picks(self):
-        pass
+        random.seed(0)
+
+        for round_number in xrange(1,5):
+            print '=== Round {} ==='.format(round_number)
+            for region in self.regions:
+                print '-- {} --'.format(region)
+                games_this_round = 16/(2**round_number)
+                for game_number in xrange(games_this_round):
+                    game_id = self.game_id(region, round_number, game_number+1)
+
+                    if round_number == 4:
+                        next_game_number = 1 if region in ['South', 'West'] else 2
+                        next_game_id = self.game_id('Final_Four', 5, next_game_number)
+                    else:
+                        next_game_id = self.game_id(region, round_number+1, game_number//2 + 1)
+
+                    self.pick_winner(game_id, next_game_id)
+            print
+
+        # Final Four
+        for round_number in xrange(5,7):
+            print '=== Final Four ==='
+            region = 'Final_Four'
+            games_this_round = 64/(2**round_number)
+            for game_number in xrange(games_this_round):
+                game_id = self.game_id(region, round_number, game_number+1)
+                next_game_id = self.game_id(region, round_number+1, game_number//2 + 1)
+                self.pick_winner(game_id, next_game_id)
+            print
+
 
 rater = EloRater()
 print 'Computing Ratings'
@@ -153,3 +232,5 @@ rater.adjust_for_strength_of_schedule()
 #rater.compute_ratings()
 rater.print_ratings()
 
+rater.generate_initial_matchups()
+rater.generate_bracket_picks()
